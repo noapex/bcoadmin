dfbad002d1190849a7e2c80925c5c08053d72e0c
Ramiro Blanco <ramiro@cinconueves.com>
2017-12-10 21:56:25 -0300

    url de debug si la app debug_toolbar esta instalada

--- bcoadmin/urls.py
+++ bcoadmin/urls.py
@@ -46,7 +46,8 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns += [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ]
